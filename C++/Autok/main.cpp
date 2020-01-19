35f198c26dd806c809fd6046020634d249bfad58
Hvik555 <60071489+Hvik555@users.noreply.github.com>
2020-01-19 18:48:54 +0100

    Update main.cpp

--- C++/Autok/main.cpp
+++ C++/Autok/main.cpp
@@ -10,45 +10,5 @@ int main()
     Menu App;
     App.Run();
 
-    /*ifstream f("Autok.txt");
-    vector<CarDetails> a;
-    CarDetails dx;
-
-
-    string brand;
-    string type;
-    int year;
-    double consumption;
-    int price;
-
-    while(f >> brand >> type >> year >> consumption >> price)
-    {
-        cout << brand << " " << type << " " << year << " " << consumption << " " << price << endl;
-    }
-
-    while(f >> dx)
-    {
-        a.push_back(dx);
-
-    }
-
-    CarCont vec(a);
-
-    CarCont vec("Autok.txt");
-    CarCont a;
-
-    vec.print();
-
-    cout << endl;
-
-
-    int km,ev,evjarat;
-    string marka,tipus;
-
-    cout << "Kerem adja meg a km,hany evre,markat,tipust,evjaratot" << endl;
-    cin >> km >> ev >> marka >> tipus >> evjarat;
-
-    cout << vec.AvgConsumption(km,ev,marka,tipus,evjarat);*/
-
     return 0;
 }
